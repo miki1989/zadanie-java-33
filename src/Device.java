e61a3c871ebdbdf096889afdf399d596a6fddf55
miki1989 <mikolajheller@gmail.com>
2018-07-07 16:43:05 +0200

    Changes in version - added constructors in Device, added method getInfo(), corrected "smartyphone" to smartphone.

---
+++ src/Device.java
@@ -0,0 +1,26 @@
+public class Device {
+    String code;
+    String producer;
+    String type;
+    double price;
+
+    Device (String code, String producer, String type, double price)
+    {
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+        this.price = price;
+    }
+
+    Device (String code, String producer, String type)
+    {
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+    }
+
+    String getInfo()
+    {
+        return ("Kod: " + code + ", producent: " + producer + ", typ: " + type + ", cena " + price);
+    }
+}
